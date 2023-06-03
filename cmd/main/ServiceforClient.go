b30011942c09763e45a81d9114ded184471d63b6
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-06-04 00:20:12 +0800

    import watch package to main and deploy

--- cmd/main/ServiceforClient.go
+++ cmd/main/ServiceforClient.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"time"
 
+	"distributed_finalproject/watch"
+
 	api "go.etcd.io/etcd/api/etcdserverpb"
 	client "go.etcd.io/etcd/client/v3"
 )
@@ -25,13 +27,34 @@ func main() {
 
 	c, _ := client.New(*config)
 
+	defer c.Close()
+
 	//translate to RangeResponse which have method to access value (write a independent func later)
 	r, _ := c.Get(c.Ctx(), "test")
 	fmt.Println(getResponse_process(r))
 
-	watcher := client.NewWatcher(c) //return a channel
-	wac := <-watcher.Watch(ctx, "test")
-	fmt.Println(watchResponse_process(wac))
+	watcher := c.Watcher
+	defer watcher.Close()
+
+	watches := watch.New_watchs(watcher, 5)
+
+	watch.Add_watch_key("test", watches, ctx)
+	watch.Add_watch_key("foo", watches, ctx)
+	//test for channel of channel
+	for {
+		select {
+		case val := <-watches.Watch_chaninfo:
+			if check_watch_table(*watches, val.C_name) {
+				select {
+				case vall := <-val.Cn:
+					watches.Watch_chaninfo <- val
+					fmt.Println(watchResponse_process(vall))
+				default:
+					watches.Watch_chaninfo <- val
+				}
+			}
+		}
+	}
 
 }
 
@@ -42,5 +65,19 @@ func getResponse_process(r *client.GetResponse) string {
 }
 
 func watchResponse_process(r client.WatchResponse) string {
+	fmt.Println(r)
 	return string(r.Events[0].Kv.Value)
+}
+
+func watch_key_c(key string, ctx context.Context, watcher client.Watcher, c chan client.WatchChan) {
+	c <- watcher.Watch(ctx, key)
+}
+
+func check_watch_table(watches watch.Watchs, key string) bool {
+	for i := 0; i < len(watches.Cache_t); i++ {
+		if watches.Cache_t[i] == key {
+			return true
+		}
+	}
+	return false
 }
